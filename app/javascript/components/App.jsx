4ae9779c76297d9cabc13d58f4007e33b31fa717
MargaretOm <margosha20617@gmail.com>
2019-08-06 18:10:55 +0300

    Add components for sign_in and sign_up
    Use axios to perform HTTP requests
    Create forms for sign_in and sign_up

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
 import {HashRouter as Router, Route, NavLink, Switch} from 'react-router-dom'
 
 class App extends Component {
@@ -22,6 +24,9 @@ const Navigation = () => (
     <ul className="navbar-nav mr-auto">
       <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/home">Home</NavLink></li>
       <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/">Back</NavLink></li>
+      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/sign_up">Sign up</NavLink></li>
+      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/sign_in">Sign in</NavLink></li>
+      {/* <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/???">Sign out</NavLink></li> */}
     </ul>
   </nav>
 );
@@ -30,6 +35,8 @@ const Main = () => (
   <Switch>
     <Route exact path="/" />
     <Route path="/home" component={Home} />
+    <Route path="/sign_up" component={SignUp} />
+    <Route path="/sign_in" component={SignIn} />
   </Switch>
 );
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '*page', to: 'static#index',
                constraints: ->(req) { !req.xhr? && req.format.html? }
+  root 'welcome#index'
+  mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
