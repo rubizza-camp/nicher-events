fa52f1c1225ed8e6c0e3f42dbfac07b3064be832
kos1kov <dima.kosikov00@mail.ru>
2019-08-07 15:32:20 +0300

    Make tests for sign_up, sign_in and sign_out
    Add seed with user
    Delete unless sign_out file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = [
+    { email: 'user@gmail.com', password: '123456' }
+]
+
+User.create(users)
