055d317be4621d4ae39f5a9b402e250327123b19
kos1kov <dima.kosikov00@mail.ru>
2019-08-09 15:13:38 +0300

    Add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
   get '*page', to: 'static#index',
                constraints: ->(req) { !req.xhr? && req.format.html? }
   mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
