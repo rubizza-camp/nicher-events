0cee152bea1d054c37606859d6f72c34edd0ecf5
SvintBel <40833597+svintbel@users.noreply.github.com>
2019-08-09 16:09:25 +0300

    Add config changes for Heroku (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,8 @@ Rails.application.routes.draw do
       resources :users
     end
   end
+
+  get '*page', to: 'static#index',
+               constraints: ->(req) { !req.xhr? && req.format.html? }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
