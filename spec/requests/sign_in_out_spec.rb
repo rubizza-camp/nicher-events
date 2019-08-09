31b7f2e78001065c74e696499594bec3a46395ab
kos1kov <dima.kosikov00@mail.ru>
2019-08-09 16:48:00 +0300

    Separate test into context

--- spec/requests/sign_in_out_spec.rb
+++ spec/requests/sign_in_out_spec.rb
@@ -3,38 +3,56 @@ require 'rails_helper'
 describe 'SignInOut', type: :request do
   include ActionController::RespondWith
   let(:user) { create(:user) }
-  it 'should get success status' do
-    get '/#/sign_in'
-    expect(response).to have_http_status(:success)
 
-    post '/auth/sign_in', params: { email: user.email, password: user.password }
-    expect(response).to have_http_status(:success)
+  context 'should get success status when sign_in' do
+    it 'get sign_in form' do
+      get '/#/sign_in'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'with right params' do
+      post '/auth/sign_in', params: { email: user.email, password: user.password }
+      expect(response).to have_http_status(:success)
+    end
   end
 
-  it 'should get error status, because incomplete data or incorrect data' do
-    post '/auth/sign_in', params: { password: user.password }
-    expect(response).to have_http_status(401)
+  context 'get error status when sign_in' do
+    it 'fill only password' do
+      post '/auth/sign_in', params: { password: user.password }
+      expect(response).to have_http_status(401)
+    end
 
-    post '/auth/sign_in', params: { email: user.email }
-    expect(response).to have_http_status(401)
+    it 'fill only email' do
+      post '/auth/sign_in', params: { email: user.email }
+      expect(response).to have_http_status(401)
+    end
 
-    post '/auth/sign_in', params: { email: user.password, password: user.email }
-    expect(response).to have_http_status(401)
+    it 'email and password are swapped' do
+      post '/auth/sign_in', params: { email: user.password, password: user.email }
+      expect(response).to have_http_status(401)
+    end
   end
 
-  it 'should get success status' do
-    get '/'
-    expect(response).to have_http_status(:success)
+  context 'should get success status when sign_out' do
+    it 'get main page' do
+      get '/'
+      expect(response).to have_http_status(:success)
+    end
 
-    delete '/auth/sign_out', params: user.create_new_auth_token
-    expect(response).to have_http_status(:success)
+    it 'with right token' do
+      delete '/auth/sign_out', params: user.create_new_auth_token
+      expect(response).to have_http_status(:success)
+    end
   end
+  context 'should get error status when sign_out' do
+    it 'without params' do
+      delete '/auth/sign_out'
+      expect(response).to have_http_status(404)
+    end
 
-  it 'should get error status, because empty data or incorrect data' do
-    delete '/auth/sign_out'
-    expect(response).to have_http_status(404)
-
-    delete '/auth/sign_out', params: {}
-    expect(response).to have_http_status(404)
+    it 'incorrect params' do
+      delete '/auth/sign_out', params: {}
+      expect(response).to have_http_status(404)
+    end
   end
 end
