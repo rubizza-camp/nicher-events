eaa45cdd0390a90229122b043c470a33d788da10
kos1kov <dima.kosikov00@mail.ru>
2019-08-09 16:41:24 +0300

    Delete unused line from db

--- db/migrate/20190805091526_devise_token_auth_create_users.rb
+++ db/migrate/20190805091526_devise_token_auth_create_users.rb
@@ -34,8 +34,6 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       t.string :first_name
       t.string :last_name
       t.string :phone
-      t.string :nickname
-      t.string :image
       t.string :email
 
       ## Tokens
@@ -48,6 +46,5 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,       unique: true
   end
 end
