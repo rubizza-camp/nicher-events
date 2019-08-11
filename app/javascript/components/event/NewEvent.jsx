25bd651f9cfe7ac78cdf0b11bbdfee962eb155e9
Julia <lugina.yulya@mail.ru>
2019-08-11 16:24:30 +0300

    Create a separate component for event form, nove logic from return part

--- app/javascript/components/event/NewEvent.jsx
+++ app/javascript/components/event/NewEvent.jsx
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import EventForm from './EventForm';
 
 export default class NewEvent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: '', description: '', status: '', date: '' };
-    this.handleChange = this.handleChange.bind(this);
+    this.state = { event: {name: '', description: '', status: '', date: ''} };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    Axios.post('api/v1/events', this.state)
-      .then(response => {
-        this.props.history.push(`/events/${response.data.id}`);
-      })
+    Axios.post('/api/v1/events', this.state.event)
+      .then(response => this.props.history.push(`/events/${response.data.id}`))
       .catch(error => console.log('error', error))
-  }
-
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
   }
 
   handleCancel = () => {
@@ -31,31 +25,7 @@ export default class NewEvent extends React.Component {
     return (
       <div>
         <h1>Create</h1>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor='name'>Name</label> <br/>
-            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} className="form-control" />
-          </div>
-          <div className="form-group">
-            <label htmlFor='date'>Date</label> <br/>
-            <input type="datetime-local" name="date" value={this.state.date} onChange={this.handleChange}/>
-          </div>
-          <div className="form-group">
-            <label htmlFor='status'>Status : </label> <br/>
-            <input type="radio" id='status_social' name="status" onChange={this.handleChange} value="social" />
-            <label>Social</label> <br/>
-            <input id='status_confidential' type="radio" name="status" onChange={this.handleChange} value="confidential" />
-            <label>Confidential</label>
-          </div>
-          <div className="form-group">
-            <label htmlFor='description'>Description</label> <br/>
-            <textarea name="description" rows="5" value={this.state.description} onChange={this.handleChange} className="form-control" />
-          </div>
-          <div className="btn-group">
-            <button type="submit" className="btn btn-dark">Create</button>
-            <button type="button" onClick={this.handleCancel} className="btn btn-secondary">Cancel</button>
-          </div>
-        </form>
+        <EventForm event={this.state.event} handleSubmit={this.handleSubmit} handleCancel={this.handleCancel}/>
       </div>
     );
   }
