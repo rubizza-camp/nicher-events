d9986e5c1e53355b9adac9dfe658ab03d1c68e0b
Julia <lugina.yulya@mail.ru>
2019-08-12 18:27:16 +0300

    Update logic of tests generate tests for checking user role

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -4,24 +4,47 @@ RSpec.describe Api::V1::EventsController, type: :controller do
   let!(:event_attributes) { %w[id name date description status user_id] }
 
   describe 'GET #index' do
-    it 'should get index' do
-      get :index
-      expect(response).to have_http_status(:success)
+    let!(:events) { create_list(:event, 3) }
+
+    context 'when user is organizer' do
+      it 'returns json with list of all events' do
+        get :index
+        expect(json_response.first.keys).to eq(event_attributes)
+        expect(json_response.dig(0, 'name')). to eq(events[0].name)
+        expect(json_response.dig(1, 'name')). to eq(events[1].name)
+        expect(json_response.dig(2, 'name')). to eq(events[2].name)
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when user is attendee' do
+      it 'returns json with list of public and conforming private events' do
+      end
+    end
+
+    context 'when unregistered user' do
+      it 'returns json with list of public events' do
+      end
     end
   end
 
   describe 'GET #show' do
-    context 'when valid' do
-      let(:event) { create(:event) }
+    context 'when params is valid' do
+      context 'status event is public' do
+        let(:event) { create(:event, status: :social) }
 
-      it 'returns json response with event' do
-        get :show, params: { id: event.id }
-        expect(json_response.keys).to eq(event_attributes)
-        expect(json_response['name']).to eq(event.name)
+        it 'returns json response with event' do
+          get :show, params: { id: event.id }
+          expect(json_response.keys).to eq(event_attributes)
+          expect(json_response['name']).to eq(event.name)
+          expect(json_response['description']).to eq(event.description)
+          expect(json_response['date']).to eq(Time.parse(event.date.to_s).strftime('%Y-%d-%mT%H:%M'))
+          expect(json_response['status']).to eq(event.status)
+        end
       end
     end
 
-    context 'when invalid' do
+    context 'when params is invalid' do
       it 'returns not_found status' do
         get :show, params: { id: -1 }
         expect(json_response['status']).to eq('not_found')
@@ -30,42 +53,71 @@ RSpec.describe Api::V1::EventsController, type: :controller do
   end
 
   describe 'POST #create' do
-    context 'when valid' do
-      let(:valid_event) { build(:event) }
+    context 'when user is organizer' do
+      context 'when params is valid' do
+        let(:valid_event) { build(:event) }
 
-      it 'returns json with params of created event' do
-        post :create, params: { event: valid_event.attributes }
-        expect(json_response.keys).to eq(event_attributes)
-        expect(json_response['name']).to eq(valid_event.name)
+        it 'returns json with params of created event' do
+          post :create, params: { event: valid_event.attributes }
+          expect(json_response.keys).to eq(event_attributes)
+          expect(json_response['name']).to eq(valid_event.name)
+        end
+      end
+
+      context 'when params is invalid' do
+        let(:invalid_event) { build(:event, name: nil, description: 'too short', date: nil, status: nil) }
+
+        it 'returns message errors' do
+          post :create, params: { event: invalid_event.attributes }
+          expect(json_response[0]).to eq('Name can\'t be blank')
+          expect(json_response[1]).to eq('Description is too short (minimum is 10 characters)')
+          expect(json_response[2]).to eq('Date can\'t be blank')
+          expect(json_response[3]).to eq('Status is not included in the list')
+        end
       end
     end
 
-    context 'when invalid' do
-      let(:invalid_event) { build(:event, name: nil, description: 'too short') }
-
-      it 'returns message errors' do
-        post :create, params: { event: invalid_event.attributes }
-        expect(json_response[0]).to eq('Name can\'t be blank')
-        expect(json_response[1]).to eq('Description is too short (minimum is 10 characters)')
+    context 'when user is attendee or unregistered' do
+      it 'can\'t be enable' do
       end
     end
   end
 
-  describe 'GET #destroy' do
-    context 'when valid' do
-      let(:valid_event) { create(:event) }
+  describe 'DELETE #destroy' do
+    context 'when user is a creator of this event' do
+      context 'when params is valid' do
+        let(:valid_event) { create(:event) }
 
-      it 'returns params of deleted event' do
-        get :destroy, params: { id: valid_event.id }
-        event = assigns(:event)
-        expect(valid_event.name).to eq(event.name)
+        it 'returns no_content status' do
+          get :destroy, params: { id: valid_event.id }
+          event = assigns(:event)
+          expect(valid_event.name).to eq(event.name)
+          expect(response).to have_http_status(:no_content)
+        end
+      end
+
+      context 'when params is invalid' do
+        it 'returns not_found status' do
+          get :destroy, params: { id: -1 }
+          expect(json_response['status']).to eq('not_found')
+        end
       end
     end
 
-    context 'when invalid' do
-      it 'returns not_found status' do
-        get :destroy, params: { id: -1 }
-        expect(json_response['status']).to eq('not_found')
+    context 'when user isn\'t creator of this event' do
+      it 'returns errors' do
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'when user is organizer' do
+      it 'returns json with params of updated event' do
+      end
+    end
+
+    context 'when user is attendee or unregistered' do
+      it 'can\'t be enable' do
       end
     end
   end
