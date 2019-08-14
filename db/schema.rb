efa38f5fc3146de97f2518939ef149eb95cd4de2
svintbel <svintbel@gmail.com>
2019-08-14 12:48:07 +0300

    Update db directory

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_05_091526) do
+ActiveRecord::Schema.define(version: 2019_08_08_100943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "date", null: false
+    t.text "description", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -34,10 +51,19 @@ ActiveRecord::Schema.define(version: 2019_08_05_091526) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "address", default: "", null: false
+    t.text "description", default: ""
+    t.integer "people_capacity", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+require 'faker'
+
+20.times do |i|
+	Venue.create({address: Faker::Address.full_address,
+                description: Faker::Lorem.sentence,
+                people_capacity: rand(1...99)})
+end
+
+puts 'Created 20 venues'
 
 users = [
+  {
+    first_name: 'Test',
+    last_name: 'Testuser',
+    email: 'test@test.com',
+    phone: '+375291111111',
+    password: 'testtest'
+  },
+
   {
     first_name: 'Margo',
     last_name: 'Omelchuk',
