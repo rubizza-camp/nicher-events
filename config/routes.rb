a63c76d72eaf24aa797d4b0c1f4ef87562261cf7
svintbel <svintbel@gmail.com>
2019-08-14 12:46:38 +0300

    Update config directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :venues
+    end
+  end
+
   get '*page', to: 'static#index',
                constraints: ->(req) { !req.xhr? && req.format.html? }
   mount_devise_token_auth_for 'User', at: 'auth'
