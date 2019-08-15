602779fe1d43873cb11dbd9a89addc36f9aeea4d
MargaretOm <margosha20617@gmail.com>
2019-08-15 12:26:50 +0300

    Add layouts in react
    Delete Home component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import { Router, Route, NavLink, Switch} from 'react-router-dom'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -8,33 +7,11 @@ export default class App extends React.Component {
   }
 
   render() {
-    // let user_name = {first_name: '', last_name: '', role: ''};
-    // if (sessionStorage.user_attributes != null) {
-    //   user_name = JSON.parse(sessionStorage.user_attributes);
-    // }
-
-
-
     return (
       <div className="App">
-        <Navigation/>
-        { sessionStorage.user }
         <div className="container">
         </div>
       </div>
     );
   }
 }
-
-
-const Navigation = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item">
-        <NavLink exact className="nav-link" activeClassName="active" to="/home">Home</NavLink>
-      </li>
-      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/">Back</NavLink></li>
-    </ul>
-  </nav>
-);
-
