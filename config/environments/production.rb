10fe239855e657f42cf715911dbf67b54c26349a
MargaretOm <margosha20617@gmail.com>
2019-08-16 15:28:59 +0300

    Add gem dotenv

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'myherokuapp.herokuapp.com' }
+  Dotenv.load
+  config.action_mailer.default_url_options = { host: ENV['PRODUCTION_HOST' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
