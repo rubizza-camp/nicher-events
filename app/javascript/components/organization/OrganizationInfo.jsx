1c051563ee03e0c2f23c4cf2ab23e5f1ba73bf19
M1ha11 <tedpoolioo@gmail.com>
2019-08-17 07:39:00 +0300

    Add styles

--- app/javascript/components/organization/OrganizationInfo.jsx
+++ app/javascript/components/organization/OrganizationInfo.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 class OrganizationInfo extends Component {
   constructor(props) {
@@ -35,6 +42,58 @@ class OrganizationInfo extends Component {
     const editOrganizationUrl = `/organizations/${this.state.organization.id}/edit`
     const organizationListUrl = '/organizations';
     let errorsMessage;
+
+    const useStyles = makeStyles({
+      card: {
+        minWidth: 275,
+        width: 700,
+      },
+      bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+      },
+      title: {
+        fontSize: 14,
+      },
+      pos: {
+        marginBottom: 12,
+      },
+    });
+
+    const EditButton = () => {
+      const classes = useStyles();
+      return <Button component={Link} to={editOrganizationUrl} type="submit"
+              variant="contained" color="primary" className={classes.button}>Edit</Button>
+    }
+
+    const CancelButon = () => {
+      const classes = useStyles();
+      return <Button component={Link} to={organizationListUrl} type="submit"
+              variant="contained" color="primary" className={classes.button}>Cancel</Button>
+    }
+
+    const OrganizationCard = () => {
+      const classes = useStyles();
+      return (<Card className={classes.card}>
+                <CardContent>
+                  <Grid container direction="column" justify="center" alignItems="center">
+                    <Typography variant="h5" component="h2">
+                      {this.state.organization.name}
+                    </Typography>
+                  </Grid>
+                   <Typography variant="body2" component="p">
+                     {this.state.organization.description}
+                   </Typography>
+                </CardContent>
+                <CardActions>
+                  <EditButton />
+                  <CancelButon />
+                </CardActions>
+              </Card>
+        )
+    }
+
     if (this.state.errors)  {
       errorsMessage = <div>
         {this.state.errors.map((error) => {
@@ -46,15 +105,10 @@ class OrganizationInfo extends Component {
       }
     return (
       <div>
-        <p>{errorsMessage}</p>
-        <h3>{this.state.organization.name}</h3>
-        <p>{this.state.organization.description}</p>
-        <p>
-          <Link to={editOrganizationUrl}
-           className='btn btn-outline-dark'>Edit</Link>
-          <Link to={organizationListUrl}
-           className='btn btn-outline-dark'>Cancel</Link>
-        </p>
+        <Grid container direction="column" justify="center" alignItems="center">
+          <p>{errorsMessage}</p>
+          <OrganizationCard />
+        </Grid>
       </div>
     );
   }
--- app/javascript/components/organization/OrganizationList.jsx
+++ app/javascript/components/organization/OrganizationList.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
 class OrganizationList extends Component {
   constructor() {
@@ -18,36 +27,77 @@ class OrganizationList extends Component {
       }
       headers = JSON.parse(sessionStorage.user);
     }
-    axios.get('/api/v1/organizations', { headers: headers, params: sessionStorage.user })// authenticateUser })
+    axios.get('/api/v1/organizations', { headers: headers, params: sessionStorage.user})
       .then(res => {this.setState({organizations: res.data})})
       .catch(error => console.log('error', error))
   }
 
   componentDidMount() {
-    this.avaliableOrganizations()
+    this.avaliableOrganizations();
   }
 
   render() {
+      const useStyles = makeStyles(theme => ({
+      root: {
+        width: '90%',
+        marginTop: theme.spacing(3),
+        overflowX: 'auto',
+      },
+      table: {
+        minWidth: 650,
+      },
+      button: {
+      margin: theme.spacing(1),
+      width: 165,
+      },
+    }));
+
     const newOrganizationUrl = '/organizations/new'
     let createOrganizaton;
+
+    const CreateButton = () => {
+      const classes = useStyles();
+      return <Button component={Link} to={newOrganizationUrl} type="submit" variant="contained" color="primary" className={classes.button}>Create Organization</Button>
+    }
+    
     if(sessionStorage.user != null) {
       const userRole = JSON.parse(sessionStorage.user_attributes).role;
       if(userRole === 'organizer') {
-        createOrganizaton = <Link to={newOrganizationUrl} className="btn btn-outline-primary">Create Organization</Link>
+        createOrganizaton = <CreateButton />
       }
     }
+
+    const OrganizationTable = () => {
+      const classes = useStyles();
+      return (
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead className={classes.head}>
+              <TableRow>
+                <TableCell><h3>Name</h3></TableCell>
+                <TableCell align="left"><h3>Description</h3></TableCell>
+              </TableRow>
+            </TableHead>
+          <TableBody>
+            {this.state.organizations.map((organization) => (
+              <TableRow key={organization.name}>
+                <TableCell component="th" scope="row">
+                  <h2><Link to={`/organizations/${organization.id}`}>{organization.name}</Link></h2>
+                </TableCell>
+                <TableCell align="left">{organization.description.substr(0,74)}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          </Table>
+        </Paper>
+    );}
     return (
-      <div>
-        {this.state.organizations.map((organization) => {
-          return(
-            <div key={organization.id}>
-              <h2><Link to={`/organizations/${organization.id}`}>{organization.name}</Link></h2>
-              <p>{organization.description}</p>
-              <hr/>
-            </div>
-          )
-        })}
+       <div>
+        
         <div>{createOrganizaton}</div>
+        <Grid container direction="column" justify="center" alignItems="center">
+          <OrganizationTable />
+        </Grid>
       </div>
     );
   }
