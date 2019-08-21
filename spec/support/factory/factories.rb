260454e8e3f18a15461cbfcac91351619a3f3289
Julia <lugina.yulya@mail.ru>
2019-08-21 16:26:46 +0300

    Divide factories on files

--- spec/support/factory/factories.rb
+++ spec/support/factory/factories.rb
@@ -8,12 +8,4 @@ FactoryBot.define do
     phone { Faker::PhoneNumber.phone_number }
     password { Faker::Internet.password }
   end
-
-  factory :event do
-    name { Faker::Lorem.word }
-    description { Faker::Lorem.sentence }
-    date { Faker::Date.between(from: 2.days.ago, to: Date.today) }
-    status { Event.statuses.keys.sample }
-    user_id { Faker::Number.number(digits: 10) }
-  end
 end
