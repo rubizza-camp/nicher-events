5c3c3475e52adcde5516771fff14ff4a3ed00db0
Valentin Kopotchel <valentin.kopotchel@cybergizer.com>
2019-09-17 06:10:40 -0400

    Second iteration fix

--- spec/controllers/comments_controller/delete_spec.rb
+++ spec/controllers/comments_controller/delete_spec.rb
@@ -8,59 +8,62 @@ RSpec.describe Api::V1::CommentsController, type: :controller do
   let!(:attendance) { create(:attendance, event_id: event.id, user_id: user.id) }
   let!(:comment_by_user) { create(:comment, user_id: user.id, event_id: event.id) }
   let!(:comment_by_organizer) { create(:comment, user_id: organizer.id, event_id: event.id) }
-
+  let(:destroy_user_comment) { delete :destroy, params: { id: comment_by_user.id, event_id: event.id } }
+  let(:destroy_invalid_comment) { delete :destroy, params: { id: -1, event_id: event.id } }
+  let(:destroy_stranger_comment) { delete :destroy, params: { id: comment_by_organizer.id, event_id: event.id } }
+  let(:destroy_organizer_comment) { delete :destroy, params: { id: comment_by_organizer.id, event_id: event.id } }
   describe 'DELETE #destroy' do
-    context 'when user add current comment' do
+    context 'when user adds current comment' do
       before do
         @header = user.create_new_auth_token
         request.headers.merge!(@header)
       end
 
-      it 'comment is deleted' do
-        expect { delete :destroy, params: { id: comment_by_user.id, event_id: event.id } }
-          .to change { Comment.all.count }.by(-1)
+      it 'can be deleted' do
+        expect destroy_user_comment
+          .to change { Comment.count }.by(-1)
         expect(response).to have_http_status(:no_content)
       end
 
-      it 'comment isn\'t found for delete' do
-        expect { delete :destroy, params: { id: -1, event_id: event.id } }.to_not change { Comment.all.count }
-        expect(response).to have_http_status(:not_found)
+      it 'can\'t delete by invalid id' do
+        expect destroy_invalid_comment.to_not change { Comment.count }
+        expect(response).to have_http_status(:forbidden)
       end
 
-      it 'comment isn\'t found for delete, comment belongs to another user' do
-        expect { delete :destroy, params: { id: comment_by_organizer.id, event_id: event.id } }
-          .to_not change { Comment.all.count }
-        expect(response).to have_http_status(:not_found)
+      it 'can\'t delete comment of other user' do
+        expect destroy_stranger_comment
+          .to_not change { Comment.count }
+        expect(response).to have_http_status(:forbidden)
       end
     end
 
-    context 'when user organizer of current event' do
+    context 'when user is organizer of current event' do
       before do
         @header = organizer.create_new_auth_token
         request.headers.merge!(@header)
       end
 
-      it 'comment is deleted' do
-        expect { delete :destroy, params: { id: comment_by_organizer.id, event_id: event.id } }
+      it 'can be deleted' do
+        expect destroy_organizer_comment
           .to change { Comment.all.count }.by(-1)
         expect(response).to have_http_status(:no_content)
       end
 
-      it 'comment isn\'t found for delete' do
-        expect { delete :destroy, params: { id: -1, event_id: event.id } }.to_not change { Comment.all.count }
-        expect(response).to have_http_status(:not_found)
+      it 'can\'t delete by invalid id' do
+        expect destroy_invalid_comment
+        expect(response).to have_http_status(:forbidden)
       end
 
-      it 'comment is deleted, comment belongs to another user' do
-        expect { delete :destroy, params: { id: comment_by_user.id, event_id: event.id } }
+      it 'can delete comment of other user' do
+        expect destroy_user_comment
           .to change { Comment.all.count }.by(-1)
         expect(response).to have_http_status(:no_content)
       end
     end
 
-    context 'when no headres' do
+    context 'when no headers' do
       it 'returns message errors' do
-        delete :destroy, params: { id: comment_by_user.id, event_id: event.id }
+        destroy_user_comment
         @errors = [['errors', ['You need to sign in or sign up before continuing.']]]
         expect(response).to have_http_status(:unauthorized)
         expect(json_response.to_set).to eq(@errors.to_set)
