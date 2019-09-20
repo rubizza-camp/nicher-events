9b3501024db4515e7432b41bfe1c2bd8f1cd20c0
kos1kov <dima.kosikov00@mail.ru>
2019-09-20 16:11:22 +0300

    Add controller for generate documentation

---
+++ app/controllers/apidocs_controller.rb
@@ -0,0 +1,41 @@
+class ApidocsController < ActionController::Base
+  include Swagger::Blocks
+
+  swagger_root do
+    key :swagger, '2.0'
+    info do
+      key :version, '1.0.0'
+      key :title, 'Swagger Nicher-events'
+      key :description, 'API that uses a nicher-events'
+     # key :termsOfService, 'http://helloreverb.com/terms/'
+      contact do
+        key :name, 'Cybergizer'
+      end
+      license do
+        key :name, 'MIT'
+      end
+    end
+    tag do
+      key :name, 'nicher'
+      key :description, 'Pets operations'
+      externalDocs do
+        key :description, 'Find more info here'
+        key :url, 'https://swagger.io'
+      end
+    end
+    key :host, 'http://localhost:3000/'
+    key :consumes, ['application/json']
+    key :produces, ['application/json']
+  end
+
+  # A list of all classes that have swagger_* declarations.
+  SWAGGERED_CLASSES = [
+      Api::V1::VenuesController,
+      Venue,
+      self,
+  ].freeze
+
+  def index
+    render json: Swagger::Blocks.build_root_json(SWAGGERED_CLASSES)
+  end
+end
